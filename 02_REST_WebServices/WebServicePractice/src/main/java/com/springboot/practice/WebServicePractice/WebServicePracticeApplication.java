56b5570cd256815a41fe785121bfb6857c83b1cd
Velmurugan Moorthy <ext_vmoorthy@falabella.cl>
2019-06-08 22:01:30 +0530

    Vel | webservices practice folder added

---
+++ 02_REST_WebServices/WebServicePractice/src/main/java/com/springboot/practice/WebServicePractice/WebServicePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.practice.WebServicePractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicePracticeApplication.class, args);
+	}
+
+}
---
+++ 02_REST_WebServices/WebServicePractice/src/test/java/com/springboot/practice/WebServicePractice/WebServicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.practice.WebServicePractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

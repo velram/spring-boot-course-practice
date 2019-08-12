c73a9a08f1cca143219c6b157cc51ec86d74f0ca
Velmurugan Moorthy <velmurugan1993@gmail.com>
2019-08-12 12:09:07 +0530

    Vel | User controller - Create user - returning creation status code

--- 02_REST_WebServices/WebServicePractice/src/main/java/com/springboot/practice/WebServicePractice/user/UserController.java
+++ 02_REST_WebServices/WebServicePractice/src/main/java/com/springboot/practice/WebServicePractice/user/UserController.java
@@ -3,14 +3,18 @@
  */
 package com.springboot.practice.WebServicePractice.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * @author JJPL278
@@ -34,12 +38,20 @@ public class UserController {
 		return userService.findUser(userId);
 	}
 	
+	//@PostMapping(path="/create-user")
 	@PostMapping(path="/create-user")
-	public void createUser(@RequestBody User inputUser) {
+	public ResponseEntity<Object> createUser(@RequestBody User inputUser) {
+		 URI createdUserURI = null;
 		if(null != inputUser) {
-			userService.createUser(inputUser);
+		 User createdUser =	userService.createUser(inputUser);
+		 createdUserURI = ServletUriComponentsBuilder
+		 .fromCurrentRequest()
+		 .path("/{id}")
+		 .buildAndExpand(createdUser.getId()).toUri();
 		}
+		return ResponseEntity.created(createdUserURI).build();
 	}
 	
+	//TODO - Delete user
 	
 }

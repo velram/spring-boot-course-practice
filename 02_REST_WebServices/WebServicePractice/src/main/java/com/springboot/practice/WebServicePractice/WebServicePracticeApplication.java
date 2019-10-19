d64b6cdef9c00f27bc6ef60e8702019ee2559994
Velmurugan Moorthy <Velmurugan@Rentomojos-MacBook-Pro-4.local>
2019-10-19 16:31:16 +0530

    Vel | Add | Internationalization hands-on | Lecture #31 to #32

--- 02_REST_WebServices/WebServicePractice/src/main/java/com/springboot/practice/WebServicePractice/WebServicePracticeApplication.java
+++ 02_REST_WebServices/WebServicePractice/src/main/java/com/springboot/practice/WebServicePractice/WebServicePracticeApplication.java
@@ -2,12 +2,40 @@ package com.springboot.practice.WebServicePractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import javax.annotation.Resource;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 @SpringBootApplication
 public class WebServicePracticeApplication {
 
+	private static final String MESSAGE_SOURCE_FILE_NAME = "messages";
 	public static void main(String[] args) {
 		SpringApplication.run(WebServicePracticeApplication.class, args);
 	}
+
+	@Bean
+	public LocaleResolver localeResolver(){
+		/* old approach
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);*/
+		AcceptHeaderLocaleResolver localeResolverObj = new AcceptHeaderLocaleResolver();
+		return localeResolverObj;
+	}
+
+/* Old approach - can be replaced with
+    spring.messages.basename property (in application.properties)
+	@Bean
+	public ResourceBundleMessageSource messageSource(){
+		ResourceBundleMessageSource messageSourceObj = new ResourceBundleMessageSource();
+		messageSourceObj.setBasename(MESSAGE_SOURCE_FILE_NAME);
+		return messageSourceObj;
+	}*/
 
 }
--- 02_REST_WebServices/WebServicePractice/src/main/java/com/springboot/practice/WebServicePractice/helloworld/HelloWorldController.java
+++ 02_REST_WebServices/WebServicePractice/src/main/java/com/springboot/practice/WebServicePractice/helloworld/HelloWorldController.java
@@ -3,9 +3,15 @@
  */
 package com.springboot.practice.WebServicePractice.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 /**
  * @author Velmurugan Moorthy
@@ -13,6 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloWorldController {
+
+	private static final String HELLO_WORLD_KEY = "hello.world.message";
+	@Autowired
+	private MessageSource messageSource;
 
 	@GetMapping(path="/hello-world")
 	public String displayHelloWorld() {
@@ -28,6 +38,25 @@ public class HelloWorldController {
 	public HelloWorldBean displayHelloWorldWithName(@PathVariable String name) {
 	
 		return new HelloWorldBean(name);
+	}
+
+	//Old approach to get locale from RequestHeader
+	// This approach is not recommended for real-time use
+	@GetMapping(path="/hello-world-internationalized")
+	public String displayInternationalizedHelloWorld(
+			@RequestHeader(name="Accept-Language", required = false) Locale inputLocale
+	){
+
+		return messageSource.getMessage(HELLO_WORLD_KEY, null, inputLocale);
+	}
+
+	/*
+	  New approach to get locale.
+	 */
+	@GetMapping(path="/hello-world-internationalized-updated")
+	public String renderInternationalizedMessage(){
+		return messageSource.getMessage(HELLO_WORLD_KEY, null,
+				LocaleContextHolder.getLocale());
 	}
 	
 }

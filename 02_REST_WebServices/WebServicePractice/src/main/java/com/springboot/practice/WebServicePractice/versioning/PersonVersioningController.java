863c13d6e28199382af2766ad64d137dfddd43ae
Velmurugan Moorthy <velmurugan1993@gmail.com>
2019-10-22 18:46:33 +0530

    Vel | Modify | Versioning - four approaches - hands-on | Lecture #41

--- 02_REST_WebServices/WebServicePractice/src/main/java/com/springboot/practice/WebServicePractice/versioning/PersonVersioningController.java
+++ 02_REST_WebServices/WebServicePractice/src/main/java/com/springboot/practice/WebServicePractice/versioning/PersonVersioningController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 
+    /*Approach #1 (versioning) - starts */
     @GetMapping("/person/v1")
     public PersonV1 fetchPersonV1(){
         return new PersonV1("Velmurugan", "Moorthy");
@@ -20,4 +21,53 @@ public class PersonVersioningController {
     public PersonV2 fetchPersonV2(){
         return new PersonV2(new Name("Velu", "Murugan"));
     }
+    /*Approach #1 (versioning) - ends */
+
+
+    /*Approach #2 (versioning) - using path parameter starts */
+    @GetMapping(value = "/person/params", params = "version=1")
+    public PersonV1 fetchPersonByPathParamV1(){
+        return new PersonV1("Velmurugan", "Moorthy");
+    }
+
+    @GetMapping(value = "/person/params", params = "version=2")
+    public PersonV2 fetchPersonByPathParamV2(){
+        return new PersonV2(new Name("Velu", "Murugan"));
+    }
+    /*Approach #2 (versioning) - ends */
+
+
+    /*Approach #3 (versioning) - starts
+    * using headers (header versioning)
+    * */
+    @GetMapping(value = "/person/headers", headers = "X-API-VERSION=1")
+    public PersonV1 fetchPersonByHeadersV1(){
+        return new PersonV1("Velmurugan", "Moorthy");
+    }
+
+    @GetMapping(value = "/person/headers", headers = "X-API-VERSION=2")
+    public PersonV2 fetchPersonByHeadersV2(){
+        return new PersonV2(new Name("Velu", "Murugan"));
+    }
+
+
+    /*Approach #3 (versioning) - ends */
+
+    /*Approach #4 (versioning) - starts
+    * using "produces"
+    * Accept-header versioning / MIME type versioning / produces-type versioning
+    * */
+
+    @GetMapping(value = "/person/headers", produces = "application/vnd.app-v1+json")
+    public PersonV1 fetchPersonByAcceptHeadersV1(){
+        return new PersonV1("Velmurugan", "Moorthy");
+    }
+
+    @GetMapping(value = "/person/headers", produces = "application/vnd.app-v2+json")
+    public PersonV2 fetchPersonByAcceptHeadersV2(){
+        return new PersonV2(new Name("Velu", "Murugan"));
+    }
+
+    /*Approach #4 (versioning) - ends */
+
 }
